88d6f8eb2338a6c5ad03a24c39a8d0dfc143f6b4
StatKeeper <gerardoreading1@gmail.com>
2026-09-08 14:46:27 -0500

    Update equivalencias.js

--- js/equivalencias.js
+++ js/equivalencias.js
@@ -1,113 +1,126 @@
 let equivalencias = [
-    { id: 1, antiguo: "123 Dabs", oficial: "123 Dabs" },
-    { id: 2, antiguo: "12Kills", oficial: "12Kills" },
-    { id: 3, antiguo: "AAAwesomeGuy", oficial: "AAAwesomeGuy" },
-    { id: 4, antiguo: "Anciano", oficial: "Anciano" },
-    { id: 5, antiguo: "AntiBuddy", oficial: "AntiBuddy" },
-    { id: 6, antiguo: "Arcy", oficial: "Arcy" },
-    { id: 7, antiguo: "B AZAR360", oficial: "B AZAR360" },
-    { id: 8, antiguo: "B. Rommel", oficial: "B. Rommel" },
-    { id: 9, antiguo: "Benums", oficial: "Benums" },
-    { id: 10, antiguo: "Bio Tine", oficial: "Bio Tine" },
-    { id: 11, antiguo: "BlackHorse", oficial: "BlackHorse" },
-    { id: 12, antiguo: "bluebitsq78", oficial: "bluebitsq78" },
-    { id: 13, antiguo: "Boat", oficial: "Boat" },
-    { id: 14, antiguo: "BullRunner", oficial: "BullRunner" },
-    { id: 15, antiguo: "Cacha", oficial: "Cacha" },
-    { id: 16, antiguo: "carlinhos", oficial: "carlinhos" },
-    { id: 17, antiguo: "cumplear", oficial: "cumplear" },
-    { id: 18, antiguo: "Dia Dia 02", oficial: "Dia Dia 02" },
-    { id: 19, antiguo: "Diego Leal", oficial: "Diego Leal" },
-    { id: 20, antiguo: "Dirty_rat", oficial: "Dirty_rat" },
-    { id: 21, antiguo: "Donald Trump", oficial: "Donald Trump" },
-    { id: 22, antiguo: "Ecbert", oficial: "Ecbert" },
-    { id: 23, antiguo: "Elraf", oficial: "Elraf" },
-    { id: 24, antiguo: "Erik Barbaro", oficial: "Erik Barbaro" },
-    { id: 25, antiguo: "Euphory", oficial: "GJ Euphory" },
-    { id: 26, antiguo: "Falco Lombardi", oficial: "Falco Lombardi" },
-    { id: 27, antiguo: "Fenix", oficial: "Fenix" },
-    { id: 28, antiguo: "Fireknight", oficial: "Fireknight" },
-    { id: 29, antiguo: "frkndmly", oficial: "frkndmly" },
-    { id: 30, antiguo: "Furan", oficial: "Furan" },
-    { id: 31, antiguo: "Gato Electoral", oficial: "Gato Electoral" },
-    { id: 32, antiguo: "Gerard", oficial: "Gerard" },
-    { id: 33, antiguo: "GGAMER", oficial: "GGAMER" },
-    { id: 34, antiguo: "Ggindu", oficial: "GGINDU" },
-    { id: 35, antiguo: "GoldiGlock", oficial: "GoldiGlock" },
-    { id: 36, antiguo: "Happy", oficial: "Happy" },
-    { id: 37, antiguo: "Hipoflash", oficial: "Hipoflash" },
-    { id: 38, antiguo: "J@@@", oficial: "J@@@" },
-    { id: 39, antiguo: "Jackey9522", oficial: "Jackey9522" },
-    { id: 40, antiguo: "JB", oficial: "JB" },
-    { id: 41, antiguo: "Jkaarem", oficial: "Jkaarem" },
-    { id: 42, antiguo: "Juliano", oficial: "Juliano" },
-    { id: 43, antiguo: "Kartoos", oficial: "Kartoos" },
-    { id: 44, antiguo: "Kficho", oficial: "[cLm] KFICHO" },
-    { id: 45, antiguo: "kimchi", oficial: "kimchi" },
-    { id: 46, antiguo: "king vitt", oficial: "king vitt" },
-    { id: 47, antiguo: "Komodo3000", oficial: "Komodo3000" },
-    { id: 48, antiguo: "La Gloire", oficial: "La Gloire" },
-    { id: 49, antiguo: "Levi", oficial: "Levi" },
-    { id: 50, antiguo: "Little duck", oficial: "Little duck" },
-    { id: 51, antiguo: "MacGyver (RubenS)", oficial: "MacGyver (RubenS)" },
-    { id: 52, antiguo: "Majestic", oficial: "Majestic" },
-    { id: 53, antiguo: "malbieri", oficial: "malbieri" },
-    { id: 54, antiguo: "Mald. Futre", oficial: "Mald. Futre" },
-    { id: 55, antiguo: "Meriats", oficial: "Meriats" },
-    { id: 56, antiguo: "Mojo jojo", oficial: "Mojo jojo" },
-    { id: 57, antiguo: "Monday 007", oficial: "Monday 007" },
-    { id: 58, antiguo: "Muendo", oficial: "Muendo" },
-    { id: 59, antiguo: "Nova_8", oficial: "Nova_8" },
-    { id: 60, antiguo: "Pako", oficial: "P@K()" },
-    { id: 61, antiguo: "Papita Huayro", oficial: "Papita Huayro" },
-    { id: 62, antiguo: "Peon", oficial: "Peon" },
-    { id: 63, antiguo: "Persie", oficial: "Persie" },
-    { id: 64, antiguo: "Peter Aio", oficial: "Peter Aio" },
-    { id: 65, antiguo: "Player", oficial: "Player" },
-    { id: 66, antiguo: "Quino", oficial: "Quino" },
-    { id: 67, antiguo: "rabuegia", oficial: "rabuegia" },
-    { id: 68, antiguo: "RedMESC", oficial: "RedMESC" },
-    { id: 69, antiguo: "Renjifo", oficial: "Renjifo" },
-    { id: 70, antiguo: "robincule", oficial: "robincule" },
-    { id: 71, antiguo: "Rocket Bunny", oficial: "Rocket Bunny" },
-    { id: 72, antiguo: "rod", oficial: "rod" },
-    { id: 73, antiguo: "rushwarwarrior", oficial: "rushwarwarrior" },
-    { id: 74, antiguo: "Santiago RC", oficial: "Santiago RC" },
-    { id: 75, antiguo: "Saturno 1343", oficial: "Saturno 1343" },
-    { id: 76, antiguo: "Saul Goodman", oficial: "Saul Goodman" },
-    { id: 77, antiguo: "Shop kepper", oficial: "Shop kepper" },
-    { id: 78, antiguo: "Speedracer 5.0", oficial: "Speedracer 5.0" },
-    { id: 79, antiguo: "Spicy License 735", oficial: "Spicy License 735" },
-    { id: 80, antiguo: "su.li18", oficial: "su.li18" },
-    { id: 81, antiguo: "Taco", oficial: "Taco" },
-    { id: 82, antiguo: "Tailfeather28", oficial: "Tailfeather28" },
-    { id: 83, antiguo: "Tartus", oficial: "Tartus" },
-    { id: 84, antiguo: "THameed", oficial: "THameed" },
-    { id: 85, antiguo: "The Punisher", oficial: "The Punisher" },
-    { id: 86, antiguo: "This is the way", oficial: "This is the way" },
-    { id: 87, antiguo: "Tlaxcoaque", oficial: "Tlaxcoaque" },
-    { id: 88, antiguo: "TR.!", oficial: "TR.!" },
-    { id: 89, antiguo: "Tres Tres Tres", oficial: "Tres Tres Tres" },
-    { id: 90, antiguo: "Trotamundos", oficial: "Trotamundos" },
-    { id: 91, antiguo: "ullaj1999", oficial: "ullaj1999" },
-    { id: 92, antiguo: "Ustukuddus", oficial: "Ustukuddus" },
-    { id: 93, antiguo: "VicRoldanG", oficial: "VicRoldanG" },
-    { id: 94, antiguo: "villager 12", oficial: "villager 12" },
-    { id: 95, antiguo: "Wag", oficial: "Wag" },
-    { id: 96, antiguo: "Wall", oficial: "Wall" },
-    { id: 97, antiguo: "Warrior Chief", oficial: "Warrior Chief" },
-    { id: 98, antiguo: "WNxkl", oficial: "WNxkl" },
-    { id: 99, antiguo: "Xchopan", oficial: "Xchopan" },
-    { id: 100, antiguo: "Yin", oficial: "Yin" },
-    { id: 101, antiguo: "yo", oficial: "yo" },
-    { id: 102, antiguo: "Your true papa", oficial: "Your true papa" }
+    { id: 1, antiguo: "123 Dabs", oficiales: ["123 Dabs"] },
+    { id: 2, antiguo: "12Kills", oficiales: ["12Kills"] },
+    { id: 3, antiguo: "AAAwesomeGuy", oficiales: ["AAAwesomeGuy"] },
+    { id: 4, antiguo: "Anciano", oficiales: ["Anciano"] },
+    { id: 5, antiguo: "AntiBuddy", oficiales: ["AntiBuddy"] },
+    { id: 6, antiguo: "Arcy", oficiales: ["Arcy"] },
+    { id: 7, antiguo: "B AZAR360", oficiales: ["B AZAR360"] },
+    { id: 8, antiguo: "B. Rommel", oficiales: ["B. Rommel"] },
+    { id: 9, antiguo: "Benums", oficiales: ["Benums"] },
+    { id: 10, antiguo: "Bio Tine", oficiales: ["Bio Tine"] },
+    { id: 11, antiguo: "BlackHorse", oficiales: ["BlackHorse"] },
+    { id: 12, antiguo: "bluebitsq78", oficiales: ["bluebitsq78"] },
+    { id: 13, antiguo: "Boat", oficiales: ["Boat"] },
+    { id: 14, antiguo: "BullRunner", oficiales: ["BullRunner"] },
+    { id: 15, antiguo: "Cacha", oficiales: ["Cacha"] },
+    { id: 16, antiguo: "carlinhos", oficiales: ["carlinhos"] },
+    { id: 17, antiguo: "cumplear", oficiales: ["cumplear"] },
+    { id: 18, antiguo: "Dia Dia 02", oficiales: ["Dia Dia 02"] },
+    { id: 19, antiguo: "Diego Leal", oficiales: ["Diego Leal"] },
+    { id: 20, antiguo: "Dirty_rat", oficiales: ["Dirty_rat"] },
+    { id: 21, antiguo: "Donald Trump", oficiales: ["Donald Trump"] },
+    { id: 22, antiguo: "Ecbert", oficiales: ["Ecbert"] },
+    { id: 23, antiguo: "Elraf", oficiales: ["Elraf"] },
+    { id: 24, antiguo: "Erik Barbaro", oficiales: ["Erik Barbaro"] },
+    { id: 25, antiguo: "Euphory", oficiales: ["GJ Euphory"] },
+    { id: 26, antiguo: "Falco Lombardi", oficiales: ["Falco Lombardi"] },
+    { id: 27, antiguo: "Fenix", oficiales: ["Fenix"] },
+    { id: 28, antiguo: "Fireknight", oficiales: ["Fireknight"] },
+    { id: 29, antiguo: "frkndmly", oficiales: ["frkndmly"] },
+    { id: 30, antiguo: "Furan", oficiales: ["Furan"] },
+    { id: 31, antiguo: "Gato Electoral", oficiales: ["Gato Electoral"] },
+    { id: 32, antiguo: "Gerard", oficiales: ["Gerard"] },
+    { id: 33, antiguo: "GGAMER", oficiales: ["GGAMER"] },
+    { id: 34, antiguo: "Ggindu", oficiales: ["GGINDU"] },
+    { id: 35, antiguo: "GoldiGlock", oficiales: ["GoldiGlock"] },
+    { id: 36, antiguo: "Happy", oficiales: ["Happy"] },
+    { id: 37, antiguo: "Hipoflash", oficiales: ["Hipoflash"] },
+    { id: 38, antiguo: "J@@@", oficiales: ["J@@@"] },
+    { id: 39, antiguo: "Jackey9522", oficiales: ["Jackey9522"] },
+    { id: 40, antiguo: "JB", oficiales: ["JB"] },
+    { id: 41, antiguo: "Jkaarem", oficiales: ["Jkaarem"] },
+    { id: 42, antiguo: "Juliano", oficiales: ["Juliano"] },
+    { id: 43, antiguo: "Kartoos", oficiales: ["Kartoos"] },
+    { id: 44, antiguo: "Kficho", oficiales: ["[cLm] KFICHO"] },
+    { id: 45, antiguo: "kimchi", oficiales: ["kimchi"] },
+    { id: 46, antiguo: "king vitt", oficiales: ["king vitt"] },
+    { id: 47, antiguo: "Komodo3000", oficiales: ["Komodo3000"] },
+    { id: 48, antiguo: "La Gloire", oficiales: ["La Gloire"] },
+    { id: 49, antiguo: "Levi", oficiales: ["Levi"] },
+    { id: 50, antiguo: "Little duck", oficiales: ["Little duck"] },
+    { id: 51, antiguo: "MacGyver (RubenS)", oficiales: ["MacGyver (RubenS)"] },
+    { id: 52, antiguo: "Majestic", oficiales: ["Majestic"] },
+    { id: 53, antiguo: "malbieri", oficiales: ["malbieri"] },
+    { id: 54, antiguo: "Mald. Futre", oficiales: ["Mald. Futre"] },
+    { id: 55, antiguo: "Meriats", oficiales: ["Meriats"] },
+    { id: 56, antiguo: "Mojo jojo", oficiales: ["Mojo jojo"] },
+    { id: 57, antiguo: "Monday 007", oficiales: ["Monday 007"] },
+    { id: 58, antiguo: "Muendo", oficiales: ["Muendo"] },
+    { id: 59, antiguo: "Nova_8", oficiales: ["Nova_8"] },
+    { id: 60, antiguo: "Pako", oficiales: ["P@K()"] },
+    { id: 61, antiguo: "Papita Huayro", oficiales: ["Papita Huayro"] },
+    { id: 62, antiguo: "Peon", oficiales: ["Peon"] },
+    { id: 63, antiguo: "Persie", oficiales: ["Persie"] },
+    { id: 64, antiguo: "Peter Aio", oficiales: ["Peter Aio"] },
+    { id: 65, antiguo: "Player", oficiales: ["Player"] },
+    { id: 66, antiguo: "Quino", oficiales: ["Quino"] },
+    { id: 67, antiguo: "rabuegia", oficiales: ["rabuegia"] },
+    { id: 68, antiguo: "RedMESC", oficiales: ["RedMESC"] },
+    { id: 69, antiguo: "Renjifo", oficiales: ["Renjifo"] },
+    { id: 70, antiguo: "robincule", oficiales: ["robincule"] },
+    { id: 71, antiguo: "Rocket Bunny", oficiales: ["Rocket Bunny"] },
+    { id: 72, antiguo: "rod", oficiales: ["rod"] },
+    { id: 73, antiguo: "rushwarwarrior", oficiales: ["rushwarwarrior"] },
+    { id: 74, antiguo: "Santiago RC", oficiales: ["Santiago RC"] },
+    { id: 75, antiguo: "Saturno 1343", oficiales: ["Saturno 1343"] },
+    { id: 76, antiguo: "Saul Goodman", oficiales: ["Saul Goodman"] },
+    { id: 77, antiguo: "Shop kepper", oficiales: ["Shop kepper"] },
+    { id: 78, antiguo: "Speedracer 5.0", oficiales: ["Speedracer 5.0"] },
+    { id: 79, antiguo: "Spicy License 735", oficiales: ["Spicy License 735"] },
+    { id: 80, antiguo: "su.li18", oficiales: ["su.li18"] },
+    { id: 81, antiguo: "Taco", oficiales: ["Taco"] },
+    { id: 82, antiguo: "Tailfeather28", oficiales: ["Tailfeather28"] },
+    { id: 83, antiguo: "Tartus", oficiales: ["Tartus"] },
+    { id: 84, antiguo: "THameed", oficiales: ["THameed"] },
+    { id: 85, antiguo: "The Punisher", oficiales: ["The Punisher"] },
+    { id: 86, antiguo: "This is the way", oficiales: ["This is the way"] },
+    { id: 87, antiguo: "Tlaxcoaque", oficiales: ["Tlaxcoaque"] },
+    { id: 88, antiguo: "TR.!", oficiales: ["TR.!"] },
+    { id: 89, antiguo: "Tres Tres Tres", oficiales: ["Tres Tres Tres"] },
+    { id: 90, antiguo: "Trotamundos", oficiales: ["Trotamundos"] },
+    { id: 91, antiguo: "ullaj1999", oficiales: ["ullaj1999"] },
+    { id: 92, antiguo: "Ustukuddus", oficiales: ["Ustukuddus"] },
+    { id: 93, antiguo: "VicRoldanG", oficiales: ["VicRoldanG"] },
+    { id: 94, antiguo: "villager 12", oficiales: ["villager 12"] },
+    { id: 95, antiguo: "Wag", oficiales: ["Wag"] },
+    { id: 96, antiguo: "Wall", oficiales: ["Wall"] },
+    { id: 97, antiguo: "Warrior Chief", oficiales: ["Warrior Chief"] },
+    { id: 98, antiguo: "WNxkl", oficiales: ["WNxkl"] },
+    { id: 99, antiguo: "Xchopan", oficiales: ["Xchopan"] },
+    { id: 100, antiguo: "Yin", oficiales: ["Yin"] },
+    { id: 101, antiguo: "yo", oficiales: ["yo"] },
+    { id: 102, antiguo: "Your true papa", oficiales: ["Your true papa"] }
 ];
 
-// Función para recuperar lista guardada o la base
+// Función para recuperar lista guardada o la base (con migración automática si existía formato antiguo 'oficial')
 function obtenerEquivalencias() {
     const guardadas = localStorage.getItem("equivalencias_michi_dm");
     if (guardadas) {
-        return JSON.parse(guardadas);
+        try {
+            let listaParseada = JSON.parse(guardadas);
+            // Migrar registros viejos que tenían 'oficial' como string único a 'oficiales' como array
+            return listaParseada.map(item => {
+                if (!item.oficiales && item.oficial) {
+                    item.oficiales = [item.oficial];
+                } else if (!item.oficiales) {
+                    item.oficiales = [];
+                }
+                return item;
+            });
+        } catch (e) {
+            return equivalencias;
+        }
     } else {
         localStorage.setItem("equivalencias_michi_dm", JSON.stringify(equivalencias));
         return equivalencias;
@@ -125,26 +138,31 @@ function renderTabla() {
     let contadorDetectados = 0;
 
     listaActual.forEach(eq => {
-        // 1. TABLA SUPERIOR: Muestra a todos
+        let ofs = eq.oficiales || (eq.oficial ? [eq.oficial] : []);
+
+        // 1. TABLA SUPERIOR: Muestra a todos con sus 5 columnas de nicks
         if (tbodyEq) {
             const tr = document.createElement("tr");
             tr.innerHTML = `
                 <td><strong>${eq.id}</strong></td>
                 <td>${eq.antiguo}</td>
-                <td>${eq.oficial}</td>
+                <td>${ofs[0] || ""}</td>
+                <td>${ofs[1] || ""}</td>
+                <td>${ofs[2] || ""}</td>
+                <td>${ofs[3] || ""}</td>
+                <td>${ofs[4] || ""}</td>
             `;
             tbodyEq.appendChild(tr);
         }
 
         // 2. TABLA INFERIOR: Muestra si ha sido corregido, es nuevo o fue revisado
-        const esCorregido = eq.antiguo !== eq.oficial;
+        const esCorregido = ofs.length > 0 && !(ofs.length === 1 && ofs[0].toLowerCase() === eq.antiguo.toLowerCase());
         const esNuevoRegistrado = eq.esNuevo;
         const esRevisado = eq.revisado;
 
         if (tbodyDet && (esCorregido || esNuevoRegistrado || esRevisado)) {
             contadorDetectados++;
             
-            // Determinar estilo según el estado
             let etiqueta = '';
             if (esCorregido) {
                 etiqueta = `<span style="padding: 4px 8px; border-radius: 4px; font-size: 0.8em; font-weight: bold; background: #d1e7dd; color: #0f5132;">✏️ Corregido</span>`;
@@ -158,7 +176,11 @@ function renderTabla() {
             tr.innerHTML = `
                 <td><strong>${eq.id}</strong></td>
                 <td>${eq.antiguo}</td>
-                <td><strong>${eq.oficial}</strong></td>
+                <td>${ofs[0] || ""}</td>
+                <td>${ofs[1] || ""}</td>
+                <td>${ofs[2] || ""}</td>
+                <td>${ofs[3] || ""}</td>
+                <td>${ofs[4] || ""}</td>
                 <td>${etiqueta}</td>
             `;
             tbodyDet.appendChild(tr);
@@ -168,7 +190,7 @@ function renderTabla() {
     if (tbodyDet && contadorDetectados === 0) {
         tbodyDet.innerHTML = `
             <tr>
-                <td colspan="4" style="text-align: center; color: #6c757d; padding: 15px;">
+                <td colspan="8" style="text-align: center; color: #6c757d; padding: 15px;">
                     No hay jugadores con corrección o revisión registrada todavía.
                 </td>
             </tr>
@@ -178,10 +200,25 @@ function renderTabla() {
 
 function guardarEquivalencia() {
     let antiguo = document.getElementById("historial-antiguo").value.trim();
-    let oficial = document.getElementById("nick-oficial").value.trim();
     
-    if (!antiguo && !oficial) {
-        alert("Por favor ingresa al menos un nombre.");
+    // Capturar hasta 5 nicks de los inputs
+    let nks = [
+        document.getElementById("nick-oficial-1") ? document.getElementById("nick-oficial-1").value.trim() : "",
+        document.getElementById("nick-oficial-2") ? document.getElementById("nick-oficial-2").value.trim() : "",
+        document.getElementById("nick-oficial-3") ? document.getElementById("nick-oficial-3").value.trim() : "",
+        document.getElementById("nick-oficial-4") ? document.getElementById("nick-oficial-4").value.trim() : "",
+        document.getElementById("nick-oficial-5") ? document.getElementById("nick-oficial-5").value.trim() : ""
+    ].filter(n => n !== "");
+
+    // Compatibilidad en caso de que aún mantengas o uses un input único temporalmente
+    let inputUnico = document.getElementById("nick-oficial");
+    if (inputUnico && inputUnico.value.trim() !== "") {
+        let valUnico = inputUnico.value.trim();
+        if (!nks.includes(valUnico)) nks.push(valUnico);
+    }
+
+    if (!antiguo && nks.length === 0) {
+        alert("Por favor ingresa al menos un nombre de historial y un nick oficial.");
         return;
     }
 
@@ -189,24 +226,26 @@ function guardarEquivalencia() {
     let esJugadorNuevo = false;
 
     if (!antiguo) {
-        antiguo = oficial;
+        antiguo = nks[0];
         esJugadorNuevo = true;
-    } else if (!oficial) {
-        oficial = antiguo;
+    } else if (nks.length === 0) {
+        nks = [antiguo];
     }
 
     const existente = listaActual.find(e => e.antiguo.toLowerCase() === antiguo.toLowerCase());
     
     if (existente) {
-        existente.oficial = oficial;
+        existente.oficiales = nks;
+        existente.oficial = nks[0]; // retrocompatibilidad
         existente.revisado = true;
     } else {
         const nuevoId = listaActual.length > 0 ? Math.max(...listaActual.map(e => e.id)) + 1 : 1;
         listaActual.push({ 
             id: nuevoId, 
             antiguo: antiguo, 
-            oficial: oficial,
-            esNuevo: esJugadorNuevo || (antiguo === oficial),
+            oficiales: nks,
+            oficial: nks[0], // retrocompatibilidad
+            esNuevo: esJugadorNuevo,
             revisado: true
         });
     }
@@ -214,8 +253,15 @@ function guardarEquivalencia() {
     // Guardar permanentemente en memoria local
     localStorage.setItem("equivalencias_michi_dm", JSON.stringify(listaActual));
 
+    // Limpiar formulario
     document.getElementById("historial-antiguo").value = "";
-    document.getElementById("nick-oficial").value = "";
+    if (document.getElementById("nick-oficial-1")) document.getElementById("nick-oficial-1").value = "";
+    if (document.getElementById("nick-oficial-2")) document.getElementById("nick-oficial-2").value = "";
+    if (document.getElementById("nick-oficial-3")) document.getElementById("nick-oficial-3").value = "";
+    if (document.getElementById("nick-oficial-4")) document.getElementById("nick-oficial-4").value = "";
+    if (document.getElementById("nick-oficial-5")) document.getElementById("nick-oficial-5").value = "";
+    if (document.getElementById("nick-oficial")) document.getElementById("nick-oficial").value = "";
+
     renderTabla();
 }
 
